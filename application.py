9f9fed5b757946731ec184398f16da29d9568477
Kenneth <ngken0995@gmail.com>
2020-05-20 21:32:16 -0400

    add two features: login and logout

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -33,11 +33,39 @@ def signUp():
         username = request.form.get("username").upper()
         password = request.form.get("password")
 
-        if db.execute("SELECT * FROM account WHERE username = :username", {"username": username}).rowcount == 0:
-            db.execute("INSERT INTO account (username, password) VALUES (:username, :password)", {"username":username, "password":password})
+        if db.execute("SELECT * FROM accounts WHERE username = :username", {"username": username}).rowcount == 0:
+            db.execute("INSERT INTO accounts (username, password) VALUES (:username, :password)", {"username":username, "password":password})
             db.commit()
             return render_template("success.html")
         else:
-            return render_template("error.html", message="Username taken.")
+            return redirect(url_for('signUp'))
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        if "user" in session:
+            return redirect(url_for("profile"))
+        
+        return render_template("login.html")
+    else:
+        username = request.form.get("username").upper()
+        password = request.form.get("password")
 
+        if db.execute("SELECT * FROM accounts WHERE username = :username AND password = :password", {"username": username, "password":password}).rowcount == 0:
+            return redirect(url_for('login')) 
+        else:
+            session["user"] = username
+            return redirect(url_for("profile"))
+
+@app.route("/profile")
+def profile():
+    if "user" in session:
+        username = session["user"]
+        return render_template("profile.html", username=username)
+    else:
+        return redirect(url_for("index"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
